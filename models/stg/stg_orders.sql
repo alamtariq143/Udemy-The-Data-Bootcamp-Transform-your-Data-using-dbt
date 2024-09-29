3ccd071697e4e8104902ff30a5ae22b65e881a5d
Tariq Alam <capgemini.ca.mishra@mercedes-benz.com>
2024-09-29 16:11:55 +0000

    added more files

--- models/stg/stg_orders.sql
+++ models/stg/stg_orders.sql
@@ -1,24 +1,29 @@
 select
---from raw_orders
-orderid,
-orderdate,
-shipdate,
-shipmode,
+--from raw orders
+o.orderid,
+o.orderdate,
+o.shipdate,
+o.shipmode,
 o.customerid,
 o.productid,
-ordersellingprice,
-ordercostprice,
---from raw_customer
-customername,
-segment,
-country,
+o.ordersellingprice,
+o.ordercostprice,
+--from raw customer
+c.customerid,
+c.customername,
+c.segment,
+c.country,
 --from raw_product
-category,
-productname,
-subcategory,
-ordersellingprice - ordercostprice as orderprofit
+p.productid,
+p.category,
+p.productname,
+p.subcategory,
+ordersellingprice - ordercostprice as orderprofit,
+{{ markup() }} as markup
 from {{ ref('raw_orders') }} as o
 left join {{ ref('raw_customer') }} as c
 on o.customerid = c.customerid
 left join {{ ref('raw_product') }} as p
 on o.productid = p.productid
+
+{{limit_data_in_dev('orderdate')}}
